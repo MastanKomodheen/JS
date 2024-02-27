d1145c21a7165f858a6c92f89380de37fb34c009
Mastan <komodheen.mastan@krazybee.com>
2024-02-27 10:17:22 +0530

    aligned

--- myapp/src/components/Usestate.jsx
+++ myapp/src/components/Usestate.jsx
@@ -1,29 +1,29 @@
-    import React, { useState } from 'react'
+import React, { useState } from 'react'
 
-    const Usestate = () => {
-        const data = "Without Use state";
-        const [userState, setUserState] = useState("")
-        const [buttonclick, setButtonClick] = useState(true)
-        const userstatedata = () => {
-            if (buttonclick) {
-                setUserState(buttonclick?"clicked user":"")
-                setUserState("button clicked")
-                setButtonClick(false)
-            }
-            else{
-                setUserState("")
-                setButtonClick(true)
-            }
+const Usestate = () => {
+    const data = "Without Use state";
+    const [userState, setUserState] = useState("")
+    const [buttonclick, setButtonClick] = useState(true)
+    const userstatedata = () => {
+        if (buttonclick) {
+            setUserState(buttonclick ? "clicked user" : "")
+            setUserState("button clicked")
+            setButtonClick(false)
         }
-        return (
-            <div>
-                <h1>useState</h1>
-                <h2>{data}</h2>
-                <h2>{userState}</h2>
-                <button onClick={userstatedata}>{buttonclick?"click me":"Resetme"}</button>
-            </div>
-
-        )
+        else {
+            setUserState("")
+            setButtonClick(true)
+        }
     }
+    return (
+        <div>
+            <h1>useState</h1>
+            <h2>{data}</h2>
+            <h2>{userState}</h2>
+            <button onClick={userstatedata}>{buttonclick ? "click me" : "Resetme"}</button>
+        </div>
 
-    export default Usestate
+    )
+}
+
+export default Usestate
