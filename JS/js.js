d8d947c6f2e7243425558e798ff3efb3301efcf8
Mastan <komodheen.mastan@krazybee.com>
2024-02-27 14:38:11 +0530

    file got addded

---
+++ JS/js.js
@@ -0,0 +1,21 @@
+//map function
+// Defination : A map method is higer-function used to iterate over elements of an array and create a array based on the result of applying a callback function
+// each element in the original array.It does not change the original array but returns the new array with the same length as the original one,
+//where each element is the result of applying the provided function to the corresponding element of the original array.
+// Example:there are passing three parameters currentValue,index,array
+// const arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// arr.map(data => console.log(data.toString(2)))//converting binary
+// arr.map((curentValue, index, array) => {
+//     console.log(curentValue, index, array)
+//     // console.log(index)
+//     // console.log(array)
+// })
+
+//ForEach
+// Defination:ForEach method is used to iterate over elements of an array and execute a provided callback function once for each element in the array in assending order
+// const numbers = [1, 2, 3, 4, 5];
+// numbers.forEach((currentvalue,index,array)=>console.log(currentvalue,index,array))
+// numbers.forEach((array,index)=>console.log(array,index))
+// numbers.forEach((currentvalue)=>console.log(currentvalue))
+// numbers.forEach((index)=>console.log(index))
+
